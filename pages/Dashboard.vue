18cf15a6a09564e317a3965369029afab250ff8f
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-05-20 14:35:45 -0400

    get User Data

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -1,30 +1,26 @@
 <script setup>
-import {ref, onBeforeMount, onMounted} from "vue"
 import {Account} from "~/utils/Client"
 import { storeToRefs } from 'pinia';
 import {useStore} from "~/store/store"
 const store = useStore();
 const {id,user} = storeToRefs(store);
-const initialLoad = ref(false);
 const loading = ref(true);
-const networks = ref([]);
-const addresses = ref([]);
 const router = useRouter();
+const account = reactive({});
 
-onMounted(async ()=> {
-    console.log(user.value)
-    if (user.value) {
-        user.value.portfolio && user.value.portfolio.length == 0 ? initialLoad.value = true : initialLoad.value = false;
-        const account = new Account(user.value.seedAddress , user.value.networks)
-        await account.init()
-        console.log(account)
-        networks.value = account.networks
-        addresses.value = account.addresses
+onBeforeMount(async ()=> {
+     if (user.value) {
+        account.value = new Account(user.value.seedAddress , user.value.networks)
+        await account.value.init()
+        await account.value.getPortfolio();
         loading.value = false;
-        
     } else {
         router.push("/")
     }
+})
+
+onMounted(async ()=> {
+    console.log("mounted", account)
     
 })
 
@@ -43,14 +39,18 @@ onMounted(async ()=> {
             </v-col>
         </v-row>
         <v-row class="d-flex justify-center">
-            <v-col sm="10" md="8" v-if="!loading" v-for="n in networks">
+            <v-col cols="auto" v-if="!loading" v-for="n in account.value.networks">
                 <v-card>
-                    <v-img :src="n.image"></v-img>
-                    <v-card-title>{{n.pretty_name}}</v-card-title>
+                    <v-card-title>
+                        <v-avatar>
+                            <v-img :src="n.image"></v-img>
+                        </v-avatar>
+                        {{n.pretty_name}}
+                    </v-card-title>
                 </v-card>
             </v-col>
             <client-only v-else>
-            <v-col ><v-progress-linear indeterminate ></v-progress-linear></v-col>
+                <v-col ><v-progress-linear indeterminate ></v-progress-linear></v-col>
             </client-only>
              <v-col cols="auto">
                 <v-card>
@@ -58,12 +58,27 @@ onMounted(async ()=> {
                         <v-list-subheader>
                             <div class="text-h4">Adresses</div>
                         </v-list-subheader>
-                        <v-list-item v-for="a in addresses">{{a}}</v-list-item>
+                        <v-list-item v-for="a in account.value.addresses">
+                            <v-list-item-heading>
+                                <strong>{{a.network}}: </strong>{{a.address}}
+                            </v-list-item-heading>
+                        </v-list-item>
                     </v-list>
                     <client-only v-else>
                         <v-progress-linear indeterminate ></v-progress-linear>
                     </client-only>
                 </v-card>
+            </v-col>
+        </v-row>
+        <v-row v-if="!loading">
+            <v-col v-for="b in account.value.balances">
+                <div class="text-body-1">{{b}}</div>
+            </v-col>
+             <v-col v-for="r in account.value.rewards">
+                <div class="text-body-1">{{r}}</div>
+            </v-col>
+            <v-col v-for="s in account.value.staked">
+                <div class="text-body-1">{{s}}</div>
             </v-col>
         </v-row>
 </NuxtLayout>
