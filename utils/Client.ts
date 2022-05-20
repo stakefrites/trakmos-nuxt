992c8a6f3ec42c820bc461b3a965aab392c637c8
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-05-20 14:36:24 -0400

    update Client

--- utils/Client.ts
+++ utils/Client.ts
@@ -39,11 +39,15 @@ const makeClient = async (rpcUrl) => {
 };
 
 
+
 export class Account {
   address: string;
   activated: string[];
   networks: any[];
-  addresses: string[];
+  addresses: any[];
+  rewards: any[];
+  staked: any[];
+  balances: any[];
   constructor(address: string, activated: string[]) {
     this.address = address;
     this.activated = activated
@@ -52,23 +56,43 @@ export class Account {
   init = async () => {
     try {
       const networks = await mapAsync(this.activated, async (network) => {
-        return await directory.getChainData(network);
+        const chains = await directory.getChains();
+        const chain = chains[network]
+        const chainData = await directory.getChainData(network);
+        return {
+          ...chain,
+          data: chainData,
+          rpc: directory.rpcUrl(network),
+          rest: directory.restUrl(network)
+        }
       })
       this.networks = networks;
       await this.getAddresses();
     } catch (error) {
-      console.log("error", error)
       return error;
     }
   };
+
+  getPortfolio = async () => { 
+    try {
+      await this.getBalances();
+      await this.getRewards();
+      await this.getStaked();
+    } catch (error) {
+      console.log(error)
+      return error;
+    }
+  }
 
   getAddresses = async () => {
     const decoded = fromBech32(this.address);
     const addresses = await mapAsync(this.networks, (network) => {
       try {
-        const address = toBech32(network.bech32_prefix, decoded.data);
-        console.log(address)
-        return address;
+        const address = toBech32(network.data.bech32_prefix, decoded.data);
+        return {
+          address,
+          network: network.name,
+        }
       } catch (error) {
         console.log(error)
       }
@@ -76,6 +100,76 @@ export class Account {
     this.addresses = addresses;
 
   };
+
+    getIbcDenoms = async (coins, client) => {
+    return await mapAsync(coins, async (c) => {
+      if (c.denom.includes("ibc")) {
+        const hash = c.denom.split("/")[1];
+        const denomTrace = await client.ibc.transfer.denomTrace(hash);
+        return {
+          denom: denomTrace.denomTrace.baseDenom,
+          amount: c.amount,
+          ibcDenom: c.denom,
+        };
+      } else {
+        return c;
+      }
+    });
+  };
+
+
+  getBalances = async () => {
+    const balances = await mapAsync(this.addresses, async (a) => {
+      const network = _.keyBy(this.networks, "name")[a.network];
+      const client = await makeClient(network.rpc);
+      const allBalances = await client.bank.allBalances(a.address);
+      const denoms = await this.getIbcDenoms(allBalances, client);
+      const parsedDenoms = denoms.map((d) => {
+        return {
+          denom: d.denom,
+          amount: parseInt(d.amount),
+          ibcDenom: d.ibcDenom ? d.ibcDenom : false,
+        };
+      });
+      return {
+        address: a.address,
+        network: a.network,
+        balances: parsedDenoms,
+      };;
+    });
+    this.balances = balances;
+  };
+
+   getStaked = async () => {
+     const staked = await mapAsync(this.addresses, async (a) => {
+      const network = _.keyBy(this.networks, "name")[a.network];
+      const client = await makeClient(network.rpc);
+      const staked = await client.staking.delegatorDelegations(a.address);
+      return {
+        staked,
+        address: a.address,
+        network: a.network,
+      };
+     });
+     this.staked= staked;
+   };
+  
+   getRewards = async () => {
+     const rewards =  await mapAsync(this.addresses, async (a) => {
+      const network = _.keyBy(this.networks, "name")[a.network];
+      const client = await makeClient(network.rpc);
+      const rewards = await client.distribution.delegationTotalRewards(
+        a.address
+      );
+       return {
+         network: a.network,
+         address: a.address,
+         rewards
+      };
+     });
+     this.rewards = rewards;
+   };
+  
 
 }
 
