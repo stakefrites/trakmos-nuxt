dd50ce3e4315545b8e595b1f1dbd9928c69e0504
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-13 12:44:49 -0400

    logout + to home

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,11 +1,26 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useStore } from "~/store/store"
+
+const store = useStore();
+const { id, user } = storeToRefs(store);
+const router = useRouter();
+
+const logOut = () => { 
+  id.value = null;
+  router.push("/");
+}
+
+const toHome = () => {
+  router.push("/");
+}
 </script>
 
 <template>
   <v-app-bar flat density="compact" color="primary">
-    <v-app-bar-title>Trakmos</v-app-bar-title>
+    <v-app-bar-title @click="toHome">Trakmos</v-app-bar-title>
     <v-spacer></v-spacer>
-    <v-btn flat color="primary" icon>
+    <v-btn @click="logOut" flat color="primary" icon>
       <v-icon>mdi-logout</v-icon>
     </v-btn>
   </v-app-bar>
