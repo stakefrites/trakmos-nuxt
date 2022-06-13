0544c87315c6e0d6df09f9cf731bb84d7c262b35
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-13 09:34:55 -0400

    login handler

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,13 @@ import { storeToRefs } from 'pinia';
 import {ref, onBeforeMount} from "vue"
 import {useStore} from "~/store/store"
 const loginType = ref(null)
-const key = ref('6287252e3153aaa43702f0cb')
+const username = ref("jean");
+const password = ref("myPassword");
 const errorMessage = ref(null);
 const router = useRouter();
 const store = useStore();
+
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 
 
 const {id, user} = storeToRefs(store);
@@ -27,15 +30,24 @@ const signIn = () => {
 }
 
 const login = async () => {
-    errorMessage.value = null
-    const {data, refresh} = await useAsyncData("loginUser",  () => $fetch(`/api/user/${key.value}`))
-    if (data.value.user) {
-        user.value = data.value.user
-        router.push("/dashboard")
-        localStorage.setItem("user", JSON.stringify(data.value.user))
-
+    errorMessage.value = null;
+    const { data} = await useFetch("/trakmos/login",{
+        method: "POST",
+        baseURL: BASE_URL,
+        body: {
+            user: {
+                username: username.value,
+                password: password.value
+            }
+        }
+    })
+    console.log("data", data.value);
+    if (data.value.status === "error") {
+        console.log(`An error has occured: ${data.value.message}`)
+        errorMessage.value = data.value.message
     } else {
-        errorMessage.value = "Invalid key!"
+        id.value = data.value.user;
+        router.push("/dashboard")
     }
 }
 </script>
@@ -45,17 +57,17 @@ const login = async () => {
         <v-row class="d-flex justify-center">
             <v-col sm="8" md="6" lg="4">
                 <v-card v-if="!loginType">
-                        <v-card-title>
-                            <h1>Login</h1>
-                        </v-card-title>
-                        <v-card-text>
-                            <div class="text-body-1">Welcome to trakmos, fren.</div>
-                            <div class="text-body-2">Are you a recurring griller? 🥩.</div>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-btn color="primary" @click="signUp">Sign up</v-btn>
-                            <v-btn variant="outlined" @click="signIn">login</v-btn>
-                        </v-card-actions>
+                    <v-card-title>
+                        <h1>Login</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <div class="text-body-1">Welcome to trakmos, fren.</div>
+                        <div class="text-body-2">Are you a recurring griller? 🥩.</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn color="primary" @click="signUp">Sign up</v-btn>
+                        <v-btn variant="outlined" @click="signIn">login</v-btn>
+                    </v-card-actions>
                 </v-card>
                 <v-card v-if="loginType == 'signIn'">
                     <v-alert type="error" v-if="errorMessage">{{errorMessage}}</v-alert>
@@ -66,9 +78,16 @@ const login = async () => {
                             <div class="text-body-2">Your stake is in your plate, waiting with warm fries. 🍟</div>
                             <v-form class="mt-10">
                                 <v-text-field
-                                    v-model="key"
-                                    label="Enter your trakmos key"
+                                    v-model="username"
+                                    label="Username"
                                     type="text"
+                                    required
+                                    color="primary">
+                                </v-text-field>
+                                 <v-text-field
+                                    v-model="password"
+                                    label="Password"
+                                    type="password"
                                     required
                                     color="primary">
                                 </v-text-field>
