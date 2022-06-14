469ac2298a605d06078601d8e4608a6c5974d4e0
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-13 23:18:03 -0400

    add new layout

---
+++ layouts/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app class="background" full-height>
+    <AppBar/>
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+ .background {
+    background-repeat: repeat;
+    background: url(../darkness.webp), #070C38;
+    background-blend-mode: lighten, normal;
+  }
+
+</style>
