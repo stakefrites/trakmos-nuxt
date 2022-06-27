d76b0e1d73db53224189b67f8fcfe74b8eac0407
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-27 18:35:48 -0400

    font test 2

---
+++ windi.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        brandon: ['Brandon'],
+        brandonlight: ['BrandonLight'],
+        anek: 'Anek'
+      },
+      colors: {
+        stake: {
+          100: '#f48c8a'
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+})
