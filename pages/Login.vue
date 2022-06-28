27700cd0878841111bbf5202bdf6c2f9c74aee90
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-27 21:50:50 -0400

    start login page

--- pages/Login.vue
+++ pages/Login.vue
@@ -79,78 +79,19 @@ const signup = async () => {
 
 <template>
   <NuxtLayout name="home">
-    <v-row class="d-flex justify-center">
-      <v-col sm="8" md="6" lg="4">
-        <MyCard v-if="!loginType" title="Login" hasActions>
-          <template v-slot:content>
-            <div class="text-body-1">Welcome to trakmos, fren.</div>
-            <div class="text-body-2">Are you a recurring griller? 🥩.</div>
-          </template>
-          <template v-slot:actions>
-            <v-btn class="myPrimaryButton" @click="isSignUp">Sign up</v-btn>
-            <v-btn class="mySecondaryButton" variant="outlined" @click="isSignIn">login</v-btn>
-          </template>
-        </MyCard>
-        <MyCard v-if="loginType === 'signIn'" title="Welcome back to trakmos, fren." hasActions>
-          <v-alert type="error" v-if="errorMessage">{{ errorMessage }}</v-alert>
-          <template v-slot:content>
-            <div class="text-body-2">Your stake is in your plate, waiting with warm fries. 🍟</div>
-            <v-form class="mt-10">
-              <v-text-field v-model="username" label="Username" type="text" required bg-color="#76EFD3"> </v-text-field>
-              <v-text-field v-model="password" label="Password" type="password" required bg-color="#76EFD3">
-              </v-text-field>
-            </v-form>
-          </template>
-          <template v-slot:actions>
-            <v-btn class="myPrimaryButton" @click="login">Login</v-btn>
-          </template>
-        </MyCard>
-        <MyCard v-if="loginType === 'signUp'" title="Sign Up" hasActions>
-          <template v-slot:content>
-            <div class="text-body-1">Hello, fellow cosmonaut.</div>
-            <div class="text-body-2">You are tired of managing all of your steaks 🥩.</div>
-            <v-form class="mt-10">
-              <v-text-field v-model="username" label="Username" type="text" required bg-color="#76EFD3" class="myInput">
-              </v-text-field>
-              <v-text-field
-                v-model="password"
-                label="Password"
-                type="password"
-                required
-                class="myInput"
-                bg-color="#76EFD3"
-              >
-              </v-text-field>
-              <v-text-field
-                v-model="confirmPassword"
-                label="Confirm your password"
-                type="password"
-                required
-                bg-color="#76EFD3"
-              >
-              </v-text-field>
-            </v-form>
-          </template>
-          <template v-slot:actions>
-            <v-btn class="myPrimaryButton" @click="signup">Sign Up</v-btn>
-          </template>
-        </MyCard>
-      </v-col>
-    </v-row>
+    <div class="flex flex-col align-center">
+      <div class="border-[#76efd3] border-10 rounded-lg px-8 py-8 flex flex-col <md:min-w-full min-w-40rem">
+        <div class="text-4xl font-brandonlight font-weight-bold text-white">Login</div>
+        <div class="mt-6 text-white text-lg font-brandonlight">Username</div>
+        <input class="caret-[#76efd3] caret-opacity-100" type="text"/>
+        <div class="mt-6 text-white text-lg font-brandonlight">Password</div>
+        <input class="caret-[#76efd3] caret-opacity-100" type="password"/>
+        <MyButton class="mt-8 align-self-end" primary text="Login"/>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
 
 <style scoped>
-.myPrimaryButton {
-  background: #070c38 !important;
-  border: 1px solid #76efd3 !important;
-  border-radius: 6px;
-  color: #76efd3;
-}
-.mySecondaryButton {
-  background: #76efd3 !important;
-  border: 1px solid #76efd3 !important;
-  border-radius: 6px;
-  color: #070c38;
-}
+
 </style>
--- windi.config.js
+++ windi.config.js
@@ -18,5 +18,5 @@ export default defineConfig({
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('windicss/plugin/forms'),]
 })
