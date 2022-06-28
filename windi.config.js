e245638f185b9cae9510297eaa29956c402c521b
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-28 10:48:58 -0400

    add icons and pattern dependencies

--- windi.config.js
+++ windi.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'windicss/helpers'
+const { transform } = require('windicss/helpers')
 
 export default defineConfig({
   theme: {
@@ -18,5 +19,26 @@ export default defineConfig({
   variants: {
     extend: {}
   },
-  plugins: [require('windicss/plugin/forms'),]
+  plugins: [
+    require('windicss/plugin/forms'),
+    require('@windicss/plugin-heropatterns')({
+      // the list of patterns you want to generate a class for
+      // the names must be in kebab-case
+      // an empty array will generate all 87 patterns
+      // patterns: ['polka-dots', 'signal',"i-like-food" ,"four-point-stars"],
+      patterns: [],
+
+      // The foreground colors of the pattern
+      colors: {
+        default: '#76efd3',
+        'blue-dark': '#000044' // also works with rgb(0,0,205)
+      },
+
+      // The foreground opacity
+      opacity: {
+        default: '0.1',
+        100: '1.0'
+      }
+    })
+  ]
 })
