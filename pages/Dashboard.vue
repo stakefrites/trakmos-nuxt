d0830e28cc2aa0004c6e4ce5a35fa99f88e646e1
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-28 10:48:08 -0400

    new-dashboard

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -1,11 +1,39 @@
 <script setup>
 import { storeToRefs } from 'pinia'
+import { FingerPrintIcon, AdjustmentsIcon, ChartPieIcon, CurrencyDollarIcon } from '@heroicons/vue/outline'
 import { useStore } from '@/store/store'
 const store = useStore()
 const { id, user, tokens } = storeToRefs(store)
 const router = useRouter()
+const selectedAccount = ref(null)
 
 const account = reactive({})
+
+const wallet = computed(()=> {
+  if (selectedAccount.value != null) {
+    const data = {
+      total: [],
+      delegations: [],
+      rewards: [],
+      balance: [],
+      unbounding: []
+    }
+    account.value.portfolios.find(p=>p.account.name === selectedAccount.value).wallets.map(w=>{
+      data.balance.push(w.tokens.balance[w.tokens.balance.length-1])
+      data.rewards.push(w.tokens.rewards[w.tokens.rewards.length-1])
+      data.delegations.push(w.tokens.delegations[w.tokens.delegations.length -1])
+      data.unbounding.push(w.tokens.unbounding[w.tokens.unbounding.length - 1])
+      data.total.push(w.tokens.total[w.tokens.total.length -1])
+
+
+    })
+    return data
+  } else if (selectedAccount.value == null) {
+    return account.value.tokens
+  }
+})
+
+
 const BASE_URL = 'https://staging.api.trakmos.app'
   const { data, error } = await useFetch(`/trakmos/account/${id.value}`, {
     method: 'GET',
@@ -15,7 +43,7 @@ const BASE_URL = 'https://staging.api.trakmos.app'
   if (error.value) {
     console.log('An error has occured!\n%j' , error.value)
   }
-  const total = ref(0);
+
 
   account.value = data.value.account
 
@@ -24,13 +52,18 @@ const tokenValue = (t) => {
   return t.amount * found.price.cad
 }
 
-data.value.account.tokens.total.map(t=> {
-  total.value += tokenValue(t)
-})
+  const total = computed(()=>{
+    let total = 0
+    wallet.value.total.map(t=> {
+      total += tokenValue(t)
+    })
+    return total
+  })
+
 
 const tokenName = (t) => {
   const found = tokens.value.find((to) => to.base === t.denom)
-  return found.name
+  return found.symbol
 }
 const tokenImage = (t) => {
   const found = tokens.value.find((to) => to.base === t.denom)
@@ -50,60 +83,50 @@ const formatCurrency = (value, currency) => {
 
 <template>
   <NuxtLayout name="home">
-    <v-row>
-      <v-col sm="10" md="8">
-        <div class="text-8xl">Dashboard</div>
-      </v-col>
-    </v-row>
-    <v-row v-if="account.value">
-      <v-col>
-        <div class="text-5xl">Accounts</div>
-        <v-card>
-          <v-col v-for="acc in account.value.accounts">
-            <div class="text-h4">{{ acc.name }}</div>
-            <div class="text-body-1"><strong>Bech32 (118):</strong> {{ acc.bech32Address }}</div>
-            <div class="text-body-1"><strong>Evmos:</strong> {{ acc.evmosAddress }}</div>
-          </v-col>
-        </v-card>
-      </v-col>
-      <v-col>
-        <div class="text-h3">{{formatCurrency(total, "cad")}}</div>
-      </v-col>
-    </v-row>
-    <v-row v-if="account.value">
-      <v-col>
-        <div class="text-h3">Tokens</div>
-        <v-table>
-          <thead>
-            <tr>
-              <td>Denom</td>
-              <td>Amount</td>
-              <td>Value</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="token in account.value.tokens.total">
-              <td>
-                <v-img width="35" v-if="tokenImage(token)" :src="tokenImage(token)"></v-img>
-              </td>
-              <td>{{ tokenName(token) }}</td>
-              <td>{{ token.amount.toFixed(2) }}</td>
-              <td>{{ formatCurrency(tokenValue(token), 'cad') }}</td>
-            </tr>
-          </tbody>
-        </v-table>
-      </v-col>
-    </v-row>
+    <div class="grid md:grid-cols-4 <md:grid-cols-1 bg-[#76efd3] rounded-lg pa-10">
+      <div class="md:col-span-4 justify-self-end my-4">
+        <select v-model="selectedAccount" class="pr-10">
+          <option :value="null">All</option>
+          <option v-for="acc in account.value.accounts" :value="acc.name">{{ acc.name }}</option>
+        </select>
+      </div>
+      <div class="md:col-span-3 font-brandonlight md:text-6xl <md:text-4xl text-black">
+        {{formatCurrency(total,'cad')}}
+      </div>
+      <div class="py-10 md:col-span-4 flex flex-col justify-center">
+        <div class="font-brandon uppercase text-3xl my-6">Holdings</div>
+        <div class="grid  md:grid-cols-4 font-brandonlight">
+          <div v-for="token in wallet.total" class="flex flex-col justify-center align-center bg-white rounded-lg py-3 ma-2">
+              <div v-if="!tokenImage(token)" class="font-brandon uppercase rounded-circle bg-white w-35px h-35px flex justify-center align-center">{{tokenName(token).slice(0,1)}}</div>
+              <v-img class="rounded-circle bg-white" width="35" v-if="tokenImage(token)" :src="tokenImage(token)"></v-img>
+              <div class="table-cell pa-3 font-bold ">{{ tokenName(token) }}</div>
+              <div class="table-cell pa-3">{{ token.amount.toFixed(2) }}</div>
+              <div class="table-cell pa-3">{{ formatCurrency(tokenValue(token), 'cad') }}</div>
+          </div>
+        </div>
+      </div>
+      <div class="font-brandon uppercase text-3xl my-6 md:col-span-4">Action items</div>
+
+      <div class="flex flex-col content-center align-center border-1 border-white rounded-lg pa-3 ma-3 bg-white">
+        <FingerPrintIcon class="w-4rem text-[#070c38]"/>
+        <div class="font-brandonlight font-weight-bold mt-6 mb-4 py-2 px-3 bg-[#76efd3] text--[#070c38] rounded-md border-1 border-[#070c38]">Accounts</div>
+      </div>
+      <div class="flex flex-col content-center align-center border-1 border-white rounded-lg pa-3 ma-3 bg-white">
+        <AdjustmentsIcon class="w-4rem text-[#070c38]"/>
+        <div class="font-brandonlight font-weight-bold mt-6 mb-4 py-2 px-3 bg-[#76efd3] text--[#070c38] rounded-md border-1 border-[#070c38]">Settings</div>
+      </div>
+      <div class="flex flex-col content-center align-center border-1 border-white rounded-lg pa-3 ma-3 bg-white">
+        <ChartPieIcon class="w-4rem text-[#070c38]"/>
+        <div class="font-brandonlight font-weight-bold mt-6 mb-4 py-2 px-3 bg-[#76efd3] text--[#070c38] rounded-md border-1 border-[#070c38]">Historical</div>
+      </div>
+      <div class="flex flex-col content-center align-center border-1 border-white rounded-lg pa-3 ma-3 bg-white">
+        <CurrencyDollarIcon class="w-4rem text-[#070c38]"/>
+        <div class="font-brandonlight font-weight-bold mt-6 mb-4 py-2 px-3 bg-[#76efd3] text--[#070c38] rounded-md border-1 border-[#070c38]">Currency</div>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
 
 <style scoped>
-.text-body-1 {
-  color: black;
-}
 
-td,
-.text-h4 {
-  color: black;
-}
 </style>
