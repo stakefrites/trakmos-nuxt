7e7b8107dba16fcbf347e783b5054783c73ef0c3
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-30 12:33:31 -0400

    separate h1

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,10 @@ onMounted(() => {
     <section class="flex flex-col h-max justify-center">
       <div>
         <h1 class="font-brandon uppercase text-8xl <md:text-4xl <md:max-w-30rem max-w-70rem text-stake-100">
-          Multi-wallet Interchain Portfolio
+          Multi-wallet
+        </h1>
+        <h1 class="font-brandon uppercase text-8xl <md:text-4xl <md:max-w-30rem max-w-80rem text-stake-100">
+          Interchain Portfolio
         </h1>
         <h2 class="font-brandonlight font-bold text-white max-w-60rem text-6xl <md:text-2xl <md:max-w-30rem mt-10">
           Monitor all your stake’s temperature in one place
@@ -23,7 +26,7 @@ onMounted(() => {
       </div>
       <div class="flex md:flex-row <md:flex-col-reverse <md:flex-col justify-space-between md:pb-30">
         <div class="flex self-center space-x-8 h-max <md:mt-15">
-          <MyButton :primary="true" text="Trak Now" :handler="()=>router.push('/login')"/>
+          <MyButton :primary="true" text="Trak Now" :handler="()=>router.push('/app/login')"/>
           <MyButton :primary="false" text="Prices" :handler="()=>router.push('/prices')" />
         </div>
         <img alt="Stake illustration" class="md:w-35rem <md:w-20rem" src="~/assets/Illustration.png" />
