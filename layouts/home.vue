e6109ac9604ff5b52d0704546ba341b29b2d2dee
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-30 17:48:46 -0400

    UI fixes & hero pattern change

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-hero-bamboo bg-repeat-round bg-[#070c38] h-screen overflow-x-hidden">
+  <div class="bg-hero-circuit-board bg-repeat-round bg-[#070c38] h-screen overflow-x-hidden">
     <AppBar />
-    <div class="md:(container mx-auto) px-4  h-full">
+    <div class="md:(container mx-auto) px-4 py-10  h-full">
       <slot />
     </div>
   </div>
--- pages/app/Dashboard.vue
+++ pages/app/Dashboard.vue
@@ -90,8 +90,8 @@ const formatCurrency = (value, currency) => {
       <p class="text-md font-brandonlight">An error occured while fetching account data.</p>
       <MyButton @click="router.push('/app/login')" class="align-self-end mt-10" small text="Back Home"/>
     </div>
-    <div v-if="!error" class="grid md:grid-cols-4 text-white <md:grid-cols-1 bg-primary-600 rounded-lg pa-10  gap-4">
-      <div class="md:col-span-4 justify-self-end my-4">
+    <div v-if="!error" class="grid md:grid-cols-4 text-white <md:grid-cols-1 bg-primary-600 rounded-xl pa-10  gap-4">
+      <div class="md:col-span-1 md:col-start-4 md:row-start-1 justify-self-end my-4">
         <select v-model="selectedAccount" class="pr-10">
           <option :value="null">All</option>
           <option v-if="account.accounts" v-for="acc in account.accounts" :value="acc.name">{{ acc.name }}</option>
