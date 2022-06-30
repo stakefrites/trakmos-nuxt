862ba102279c4aa6ecb4c14d04f0512638dbd1f7
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-06-29 22:33:52 -0400

    refactor price card

---
+++ components/Price/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+  const props = defineProps({
+    symbol: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    price: {
+      type: String
+    },
+    image:  {
+      price: String
+    }
+  })
+
+</script>
+
+<template>
+  <div class="bg-primary-400 shadow-xl rounded-xl py-5 px-3 max-w-30rem text-white flex-row flex justify-space-around align-center">
+      <div class="flex flex-row space-x-5">
+        <img alt="logo" class="w-10 h-10" :src="image"/>
+        <div class="flex flex-col">
+          <div class="font-brandon text-xl uppercase">{{props.symbol}}</div>
+          <div class="font-brandonlight capitalize -mt-1">{{ props.name }}</div>
+        </div>
+      </div>
+      <div class="font-brandon text-lg">{{props.price}}</div>
+    </div>
+</template>
