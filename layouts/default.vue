f6b9132aa1b9fce08a906dd4c64ca544a1850fa3
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-07-04 21:32:16 -0400

    ui fixes - bg repeat

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-hero-circuit-board bg-repeat-round bg-[#070c38] h-screen overflow-x-hidden">
+  <div class="bg-hero-circuit-board bg-repeat bg-[#070c38] h-screen overflow-x-hidden">
     <AppBar />
     <div class="md:(container mx-auto) px-4 py-10  h-full">
       <slot />
--- layouts/side.vue
+++ layouts/side.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="bg-hero-circuit-board bg-repeat-round bg-[#070c38] h-screen overflow-x-hidden flex <md:flex-col">
+  <div class="bg-hero-circuit-board bg-repeat bg-[#070c38] h-screen overflow-x-hidden flex <md:flex-col">
     <Sidebar/>
     <div class="md:(container mx-auto) px-4 py-10  h-full">
       <slot />
