cb2dbccba7f1b2159eaa283225a1c593526b28a4
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-07-07 01:08:04 -0400

    refactor onboarding

--- pages/app/Onboarding.vue
+++ pages/app/Onboarding.vue
@@ -1,106 +1,96 @@
-<script setup>
+<script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useStore } from "@/store/store"
-import AccountConfig from "../../components/Onboarding/AccountConfig";
+import { useClient } from "#imports";
 
 const store = useStore();
 const router = useRouter();
 const { id, currentKey } = storeToRefs(store);
+const client = useClient()
 
-const BASE_URL = process.env.BASE_URL
-const NETWORKS = ["cosmoshub", "juno", "evmos"];
 
-const accounts = reactive({
-    currency: "",
-    accounts: [{
-        name: "",
-        bech32Address: "",
-        evmosAddress: "",
-        keyUsed: "",
-    }]
-})
-
+const EVMOS_CHAIN_ID = "evmos_9001-2";
+const COSMOS_CHAIN_ID = "cosmoshub-4";
 const items = ref(['usd', 'cad', 'eur']);
 const page = ref(1);
 const alreadySet = ref(false)
 const isLoading = ref(false);
+const errorMessage = ref(null);
 
-const isAlreadySet = () => { 
+const accounts = reactive({
+    currency: "usd",
+    accounts: [{
+        name: "",
+        cosmosAddress: "",
+        evmosAddress: "",
+        keyUsed: "",
+        hasEvmos: true,
+    }]
+})
+
+
+onMounted(()=>{
+  window.addEventListener("keplr_keystorechange", setCurrentKey);
+  if (id.value === null) {
+    router.push('/app/login')
+  }
+})
+
+
+const isAlreadySet = () => {
     const isIt = accounts.accounts.findIndex(acc => acc.keyUsed === currentKey.value);
     if (!isIt) {
         alreadySet.value = false;
         return false
-    } else { 
+    } else {
         alreadySet.value = isIt;
         return isIt
     }
 }
 
-const setCurrentKey = async () => { 
+const setCurrentKey = async () => {
     await window.keplr.enable(EVMOS_CHAIN_ID);
     const key = await window.keplr.getKey(COSMOS_CHAIN_ID);
     currentKey.value = key.name;
     isAlreadySet();
 }
 
-window.addEventListener("keplr_keystorechange", setCurrentKey);
-
-const EVMOS_CHAIN_ID = "evmos_9001-2";
-const COSMOS_CHAIN_ID = "cosmoshub-4";
-
-
-
-
-
-
-
-const addOne = () => { 
-    accounts.accounts.push({
-        name: "",
-        bech32Address: "",
-        evmosAddress: ""
-    });
-}
-
-const validatePage1 = () => { 
+const validatePage1 = () => {
     if (accounts.accounts.length > 0) {
         return true
-    } else { 
+    } else {
         return false
     }
 }
 
-const page2 = () => { 
+const page2 = () => {
     const isValid = validatePage1();
     if (isValid) {
         page.value = 2;
-    } else { 
-        console.log("Page 1 is not valid")   
+    } else {
+        console.log("Page 1 is not valid")
     }
 }
 
 
 const createAccount = async () => {
     isLoading.value = true;
-    const { data, error, loading } = await useFetch(`/trakmos/account`, {
-        method: "POST",
-        baseURL: BASE_URL,
-        body: {
-            config: {
-                accounts: accounts.accounts,
-                currency: accounts.currency,
-                userId: id.value,
-                networks: NETWORKS
 
-            }
-        }
-    });
-    isLoading.value = false;
-    if (error.value) {
-        console.log("An error occured");
-    } else {
-        console.log(data);
-        router.push("/app/dashboard");
+    try {
+      if (id.value === null) {
+        router.push('/app/login')
+      } else {
+        const account = await client.mutation('account.create', {
+          accountConfig: accounts.accounts,
+          currency: accounts.currency,
+          userId: id.value,
+        })
+        console.log(account)
+        router.push('/app/dashboard')
+      }
+    } catch (e) {
+      console.error(e)
+      errorMessage.value= JSON.parse(e.message)
     }
 }
 
@@ -122,12 +112,12 @@ const createAccount = async () => {
             <div class="underline-offset-1 underline">Would you love to speed up the process with KEPLR?</div>
             <div @click="setCurrentKey" class="cursor-pointer py-2 px-4 rounded-lg bg-accent-500 text-primary-500 uppercase font-brandon w-max hover:(bg-white text-primary-500)">Connect with Keplr</div>
           </div>
-          <AccountConfig :currentKey="currentKey"/>
+          <OnboardingAccountConfig :accounts="accounts" :currentKey="currentKey"/>
           <MyButton @click="page2" small primary text="Next"></MyButton>
         </div>
         <div v-if="page ===2">
           <div class="my-10 font-brandonlight text-xl font-weight-bold">Select your currency</div>
-          <select class="font-brandonlight">
+          <select v-model="accounts.currency" class="font-brandonlight">
             <option>Select a currency</option>
             <option value="cad">CAD</option>
             <option value="eur">EUR</option>
